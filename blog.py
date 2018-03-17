68f787341c9d462072c08f962798f0f3afd0e550
sdong <sdong@ctrip.com>
2018-03-17 12:43:22 +0800

    Update spider blog logic

--- blog.py
+++ blog.py
@@ -2,23 +2,53 @@
 import requests
 from bs4 import BeautifulSoup
 import sys
+import os
 from mail import send_email_qq
+from settings import ZB
 
-if __name__ == '__main__':
+
+def spider_zb(url):
     s = requests.session()
     s.keep_alive = False
-    blog_id_path = sys.path[0] + '/blog_id'
-    with open(blog_id_path, 'r') as ff:
-        blog_id = int(ff.readlines()[0].strip())
-
-    url = 'https://www.zb.com/i/blog?item={}&type='.format(blog_id)
     resonse = requests.get(url)
     text = resonse.text
     bs = BeautifulSoup(text, 'html.parser')
     elements = bs.find_all('article')
+    return elements
+
+
+def spider_clamation():
+    url = ZB.get('platform_url') + 'i/blog?type=proclamation'
+    elements = spider_zb(url)
     if elements:
-        text = elements[0].find_all('p')
-        if text:
-            send_email_qq(str(text))
-        with open(blog_id_path, 'w') as ff:
-            ff.write(str(blog_id + 1))
+        blog_latest = elements[0]  # get the latest blog
+    else:
+        raise Exception('No blog found')
+    blog_href = blog_latest.find_all('a')[1]['href']
+    blog_title = blog_latest.find_all('a')[1].text
+    return blog_href, blog_title.strip()
+
+
+def spider_blog_detail(blog_href):
+    url = ZB.get('platform_url') + blog_href[1:]
+    elements = spider_zb(url)
+    if elements:
+        return elements[0].text
+    else:
+        raise Exception('No detail blog found')
+
+
+if __name__ == '__main__':
+    blog_path = os.path.join(sys.path[0], 'blog_latest_title')
+    with open(blog_path, 'r') as rr:
+        blog_title = rr.readlines()
+
+    try:
+        href, title = spider_clamation()
+        if not blog_title or blog_title[0] != title:
+            blog_text = spider_blog_detail(href)
+            send_email_qq(blog_text)
+            with open(blog_path, 'w+') as ww:
+                ww.write(title)
+    except Exception as e:
+        print(e)
